ceeb9c63d4a4000344744e7bdf7b4e701867a571
hanjunfeng <hanjunfeng@hanjunfeng>
2018-10-09 16:41:27 +0800

    新增模板引擎 ejs

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const Koa = require('koa')
-
-const app = new Koa();
+const app = new Koa()
+const { htmlTpl, ejsTpl } = require('./tpl')
+const ejs = require('ejs')
 
 app.use(async (ctx, next) => {
-    ctx.body = '开始使用koa吧'
+    ctx.type = 'text/html; chatset=utf-8'
+    ctx.body = ejs.render(ejsTpl, {
+        name: 'Han',
+        sex: 'Man'
+    })
 })
 
 app.listen(3000)
