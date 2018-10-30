12227b4413f61163b0a5e518fa8613c764600925
hanjunfeng <hanjunfeng@hanjunfeng>
2018-10-30 15:13:16 +0800

    update: 引入路由

--- server/index.js
+++ server/index.js
@@ -4,12 +4,13 @@ const views = require('koa-views')
 const { resolve } = require('path')
 
 const { connect, initSchemas, initAdmin } = require('./database/init')
+const router = require('./routes')
 
 ;(async () => {
     await connect()
 
     initSchemas()
-    await initAdmin()
+    // await initAdmin()
     // ---> 爬取电影粗略数据,并保存到数据库
     // require('./tasks/movie')
     // ---> 获取数据库中电影的简易数据，通过豆瓣 API 获取详细数据并分类保存
@@ -17,6 +18,10 @@ const { connect, initSchemas, initAdmin } = require('./database/init')
     // require('./tasks/trailer')
     // require('./tasks/qiniu')
 })()
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
 
 app.use(views(resolve(__dirname, './views'), {
     extension: 'pug'
